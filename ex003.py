c79be062061353edcfa225ecc045c5c8363619d7
Pedro Sanchez <64911151+PrediN@users.noreply.github.com>
2024-06-18 19:43:29 -0300

    Delete ex003.py

--- ex003.py
+++
@@ -1,8 +0,0 @@
-print('=========Bem-vindo ao somador de números!=========')
-
-x = int(input('Digite um número:'))
-y = int(input('Digite o segundo número:'))
-
-s = x+y
-
-print('Parabéns! Você concluiu o programa e o resutlado da sua soma é: {} + {} = {}!'.format(x, y, s))
